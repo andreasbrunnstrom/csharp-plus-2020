2f9162bc9c27b41c34b5dcf35e75a525aef9628d
Andreas Brunnström <best_i25@hotmail.com>
2020-08-28 09:16:56 +0200

    Daily sync

---
+++ Session03/Session03Exercise01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Session03Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] integer = new [] { 1,2,3};
+
+
+            for (var i = 0; i < integer.Length; i++)
+            {
+                var name = nameof(integer);
+                var value = integer[i];
+
+                Console.WriteLine($"Index {i} arrayen {name} har värdet: {value}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
