99cbe92061f984391750b604666702087e5e52d1
Andreas Brunnström <best_i25@hotmail.com>
2020-08-31 15:46:10 +0200

    End of day sync. TimeDate + Random.

---
+++ Session04/Session04/Session04Example05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Session04Example05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Multidimensionella arrayer
+            int[,] chessboard = new int[8, 8];
+
+            // Svartvit bild 2D
+            int width = 128;
+            int height = 128;
+            byte[,] blackWhiteImage = new byte[128, 128];
+
+            for (var y = 0; y < height; y++)
+            {
+                for(var x = 0; x < width; x++)
+                {
+                    byte  currentPixelValue = blackWhiteImage[x, y];
+                }
+            }
+
+            // 3D
+            int size = 128;
+            int[,,] voxelData = new int[size, size, size];
+
+            for (var z = 0; z < size; z++)
+            {
+                for (var y = 0; y < size; y++)
+                {
+                    for (var x = 0; x < size; x++)
+                    {
+                        int currentVoxelValue = voxelData[x, y, z];
+                    }
+                }
+            }
+
+            // Udda sidiga (Jagged) arrayer
+            int[][] jaggedArray = new int[3][];
+            { new int[] {1,3,5}, new int[] {1,4,5}, new int[] {9,8,7,5,6}};
+        }
+    }
+}
---
+++ Session04/Session04/Session04Example06/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Session04Example06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+
+            //Generic time Universal..
+            DateTime currentTimeZoneNeutralDateTime = DateTime.UtcNow;
+
+            DateTime.IsLeapYear(currentDateTime.Date.Year);
+
+            DateTime minValue = DateTime.MaxValue;
+            DateTime maxValue = DateTime.MinValue;
+
+            //Från sträng
+            var parsed = DateTime.Parse("2000-06-28");
+            //DateTime failedParsedDate = DateTime.Parse("kkk");
+
+            bool dataWasParsed = DateTime.TryParse("kkk", out parsed);
+
+            // Tiden
+            TimeSpan currentTime = currentDateTime.TimeOfDay;
+            var lowestCount = currentTime.Ticks;
+            TimeSpan createdTime = new TimeSpan(1, 2, 3);
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            timer.Stop();
+
+            // Pausa med millisec som input i metoden.
+            Thread.Sleep(1000);
+
+            DateTime today = DateTime.Today;
+            DateTime yesterday = DateTime.Today.AddDays(-1).AddSeconds(-1);
+
+            TimeSpan resultOfTwoTimes = today - yesterday;
+            TimeSpan negativeResultOfTwoTimes = yesterday - today;
+
+            // Formatera datum specifikt resultat.
+            Console.WriteLine($"{currentDateTime:yyyy-MM-dd}");
+
+            // Formatera datum på ett specifikt språk.
+            Console.WriteLine($"{currentDateTime.ToString(CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine(createdTime + " " + timer.ElapsedTicks + " " + resultOfTwoTimes);
+
+            //Slumptal , endast 1 object/instans annars blir det inte random.
+            Random randomizer = new Random();
+            int slumpTal1 = randomizer.Next(0,100);
+            int slumpTal2 = randomizer.Next(0,100);
+
+            // Experiment med slump
+            IDictionary<int, int> randomNumbersFromGlobalRandomizer = new Dictionary<int, int>(100);
+            IDictionary<int, int> randomNumbersFromLocalRandomizer = new Dictionary<int, int>(100);
+
+            for (var i = 0; i < 10000; i++)
+            {
+                int globalRandomNumber = randomizer.Next(0, 100);
+
+                if (randomNumbersFromGlobalRandomizer.ContainsKey(globalRandomNumber))
+                {
+                    randomNumbersFromGlobalRandomizer[globalRandomNumber]++;
+                }
+                else
+                {
+                    randomNumbersFromGlobalRandomizer.Add(globalRandomNumber, 1);
+                }
+
+                int localRandomNumber = new Random().Next(0, 100);
+
+                if (randomNumbersFromLocalRandomizer.ContainsKey(localRandomNumber))
+                {
+                    randomNumbersFromLocalRandomizer[localRandomNumber]++;
+                }
+                else
+                {
+                    randomNumbersFromLocalRandomizer.Add(localRandomNumber, 1);
+                }
+            }
+        }
+    }
+}
