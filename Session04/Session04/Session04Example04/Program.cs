8e7f0640e771cec11d1a53b7b328745e3eae6c04
Andreas Brunnström <best_i25@hotmail.com>
2020-08-31 14:14:15 +0200

    String manipulation

--- Session04/Session04/Session04Example04/Program.cs
+++ Session04/Session04/Session04Example04/Program.cs
@@ -21,7 +21,52 @@ namespace Session04Example04
             bool inputIsNullOrWhitespace = string.IsNullOrWhiteSpace(inputData);
 
             //Trimma bort mellanslag och andra whitespace-tecken från början och slutet.
-            string trimmedSentenced = sentence.Trim(); //TrimEnd() or TrimStart()
+            string trimmedSentenced = sentence.Trim();
+            string endTrimmedSentenced = sentence.TrimEnd();
+
+            //Det går att ange ett speciellt tecken som skall tas bort.
+            string punctuationTrimmedSentenced = endTrimmedSentenced.TrimEnd('.');
+
+            //Leta i text
+            string searchForWord = "text";
+            int indexOfText = trimmedSentenced.IndexOf(searchForWord);
+            string subString = trimmedSentenced.Substring(indexOfText, searchForWord.Length);
+
+            //ToUpper or ToLower(); språkinställningsberoende. man kan använda ToUpperInvarient(); istället.
+
+            //Särkilt tecken.
+            char oneCharacter = trimmedSentenced[0];
+
+            for (int i = 0; i < trimmedSentenced.Length; i++)
+            {
+                //Loopa igenom en sträng, tecken för tecken.
+                char currentChar = trimmedSentenced[i];
+            }
+
+            char[] trimmedSentencedCharArray = trimmedSentenced.ToCharArray();
+
+            //Modifiera data.
+
+            trimmedSentenced = new string(trimmedSentencedCharArray);
+
+            trimmedSentenced = trimmedSentenced + "...";
+            // samma sak som ovan
+            trimmedSentenced += "...";
+
+            // Sortera siffror från sträng till 001 , 002 osv
+            // 001
+            // 002
+            // 023
+            //Istället för 
+            // 1
+            // 2
+            // 23
+            // 3
+            var numberInString = "12";
+            var paddedNumberInString = numberInString.PadLeft('3','0');
+
+
+
         }
     }
 }
