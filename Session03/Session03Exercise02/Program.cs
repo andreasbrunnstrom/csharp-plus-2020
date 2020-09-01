d02023e45946bb4ff3575de252dd998895f47c46
Andreas Brunnström <best_i25@hotmail.com>
2020-09-01 13:36:54 +0200

    Mostly done, except a logic error with (totValue / arrTal.Length)

--- Session03/Session03Exercise02/Program.cs
+++ Session03/Session03Exercise02/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace Session03Exercise02
 {
@@ -6,15 +8,52 @@ namespace Session03Exercise02
     {
         static void Main(string[] args)
         {
+            // Konvertera det inmatade värdena från strängar till siffror
+            // Resultatet skall presentera:
+            // Lägsta värdet
+            // Högsta värdet
+            // Medelvärde (snitt)
+            // Programmet skall vara felhanterat
+            // Felaktiga värden får inte påverka medelvärde, lägsta eller högsta.
+
             Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
             var input = Console.ReadLine();
             var inputArray = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
 
-            foreach (var number in inputArray)
+            int?[] arrTal = new int?[inputArray.Length];
+            
+            for (int i = 0; i < inputArray.Length; i++)
             {
-                Console.WriteLine("Värdet är: " + number);
+                try
+                {
+                    arrTal[i] = Convert.ToInt32(inputArray[i]);
+                }
+                catch
+                {
+                    arrTal[i] = null;
+                    continue;
+                }              
             }
-            //Minsta,högsta och medel värde utskrift.
+            int? minValue = arrTal[0];
+            int? totValue = 0;
+            int? maxValue = arrTal[0];
+            for (int i = 0; i < arrTal.Length; i++)
+            {
+                totValue += arrTal[i];
+                if (arrTal[i] > maxValue)
+                {
+                    maxValue = arrTal[i];
+                    if (minValue == 0) { minValue = arrTal[i]; }
+                }
+                else if (arrTal[i] < minValue)
+                {
+                    minValue = arrTal[i];
+                }
+            }
+            // Logic error - average number missing when chars are inputed from user.
+            // With conversion to Decimal it becomes 0.
+            Console.WriteLine(minValue + " " + totValue/arrTal.Length + " " + maxValue);
+            Console.WriteLine();
         }
     }
 }
